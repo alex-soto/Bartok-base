b75872ad501d9672957c0176605561ad653f7a52
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-04-03 14:18:03 -0400

    Added Player.cs and updated Bartok.cs
    Finished pg. 637

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; // Enables LINQ queries
+
+public enum PlayerType{
+	human,
+	ai
+}
+
+// The individual player of the game
+// Note: Player does NOT extend MonoBehavior (or any other class)
+
+[System.Serializable] // Makes the Player class visible in the Inspector pane
+public class Player {
+
+	public PlayerType type = PlayerType.ai;
+	public int playerNum;
+
+	public List<CardBartok> hand; // The cards in this player's hand
+
+	public SlotDef handSlotDef;
+
+	// Add a card to the hand
+	public CardBartok AddCard(CardBartok eCB){
+		if (hand == null)
+			hand = new List<CardBartok> ();
+
+		// Add the card to the hand
+		hand.Add (eCB);
+
+		return (eCB);
+	}
+
+	// Remove a card from the hand
+	public CardBartok RemoveCard(CardBartok cb){
+		hand.Remove (cb);
+		return(cb);
+	}
+
+}
+
