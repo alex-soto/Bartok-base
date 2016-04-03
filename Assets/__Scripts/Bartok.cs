eaac0e3e93a50360b51de8e1e484b209d413874e
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-04-03 13:40:10 -0400

    Updated Bartok.cs

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -16,6 +16,9 @@ public class Bartok : MonoBehaviour {
 	public List<CardBartok> drawPile;
 	public List<CardBartok> discardPile;
 
+	public BartokLayout layout;
+	public Transform layoutAnchor;
+
 	void Awake(){
 		S = this;
 	}
@@ -26,5 +29,21 @@ public class Bartok : MonoBehaviour {
 		Deck.Shuffle (ref deck.cards); // Tis shuffles the deck
 		// The ref keyword passes a reference to deck.cards, which allows 
 		// deck.cards to be moified by Deck.Shuffle()
+
+		layout = GetComponent<BartokLayout>(); // Get the Layout
+		layout.ReadLayout (layoutXML.text); // Pass LayoutXML to it
+
+		drawPile = UpgradeCardsList (deck.cards);
 	}
+
+	// UpgradeCardsList casts the Cards in lCD to be CardBartoks
+	// Of course, they were all along, but this lets Unity know it
+	List<CardBartok> UpgradeCardsList(List<Card> lCD){
+		List<CardBartok> lCB = new List<CardBartok>();
+		foreach(Card tCD in lCD){
+			lCB.Add(tCD as CardBartok);
+		}
+		return (lCB);
+	}
+	
 }
