2b5fb933de34bf54ff8bce0542b884be69c48ec1
Alex Soto <as1694@scarletmail.rutgers.edu>
2016-04-03 12:48:44 -0400

    Added the Bartok, CardBartok classes. Updated Utils.cs.

---
+++ Assets/__Scripts/Bartok.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Extends card like CardProspector does
+public class Bartok : MonoBehaviour {
+	static public Bartok S;
+
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
+	public Vector3 layoutCenter = Vector3.zero;
+
+	public bool __________;
+
+	public Deck deck;
+	public List<CardBartok> drawPile;
+	public List<CardBartok> discardPile;
+
+	void Awake(){
+		S = this;
+	}
+
+	void Start(){
+		deck = GetComponent<Deck> (); // Get the deck
+		deck.InitDeck (deckXML.text); // Pass DeckXML to it
+		Deck.Shuffle (ref deck.cards); // Tis shuffles the deck
+		// The ref keyword passes a reference to deck.cards, which allows 
+		// deck.cards to be moified by Deck.Shuffle()
+	}
+}
